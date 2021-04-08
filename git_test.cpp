1f6309193ac4953549735d3d7e46200835178c3b
shiva1998-R <shiva1998r@gmail.com>
2021-04-08 18:19:43 +0530

    re

--- git_test.cpp
+++ git_test.cpp
@@ -3,16 +3,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	lets see
-	okay awesome
-	done
-=======
-	cout<<"some addun"<<endl;
->>>>>>> Stashed changes
-=======
-	cout<<"some addun"<<endl;
->>>>>>> Stashed changes
 	return 0;
 }
